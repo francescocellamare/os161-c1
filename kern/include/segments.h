7a934bc5588c4c0e3de6bc533cf6f2df0aed33e5
Francesco Cellamare <francesco.pio.cellamare28@gmail.com>
2023-11-13 10:19:26 +0000

    added segment module

---
+++ kern/include/segments.h
@@ -0,0 +1,51 @@
+#ifndef _SEGMENTS_H_
+#define _SEGMENTS_H_
+
+#include <types.h>
+
+/**
+ * (from elf.h)==========================
+ * type: 
+ *  PT_LOAD 1 Loadable program segment
+ * flags:
+ *   PF_R		0x4	Segment is readable 
+ *   PF_W		0x2	Segment is writable
+ *   PF_X		0x1	Segment is executable
+ * 
+ * if p_memsz > p_filesz, the leftover space should be zero-filled
+ * ======================================
+ * 
+ * for boundaries of the segment size:
+ *  p_vaddr < current va < p_vaddr + p_memsz 
+ * 
+*/
+struct segment {
+    uint32_t	p_type;      /* Type of segment */
+	uint32_t	p_offset;    /* Location of data within file */
+	uint32_t	p_vaddr;     /* Virtual address aka base_addr*/
+	uint32_t	p_filesz;    /* Size of data within file */
+	uint32_t	p_memsz;     /* Size of data to be loaded into memory*/
+	uint32_t	p_permission;   
+
+    // TODO: page table pointer should be added or made global as proc struct
+
+    /*  
+    flow is: 
+        miss -> look for boundaries* -> find the segment -> look over pt 
+            -> !found, use the address's offset to retrieve data from base_addr + offset
+            -> found, use the entry
+        
+        *boundaries check: for each segment, check if vaddr which missed is inside base_addr and base_addr+p_memsz
+    */
+};
+
+
+
+// for each define operation over a segment checks for avoiding multi-define operations are performed by using KASSERT 
+
+struct segment* seg_create(void);
+int seg_define(struct segment* seg, uint32_t p_type, uint32_t p_offset, uint32_t p_vaddr, uint32_t p_filesz, uint32_t p_memsz, uint32_t p_permission);
+void seg_destroy(struct segment*);
+int seg_define_stack(struct segment*);
+
+#endif
---
+++ kern/vm/segments.c
@@ -0,0 +1,94 @@
+#include <types.h>
+#include <kern/errno.h>
+#include <lib.h>
+#include <spl.h>
+#include <cpu.h>
+#include <spinlock.h>
+#include <proc.h>
+#include <current.h>
+#include <mips/tlb.h>
+#include <addrspace.h>
+#include <vm.h>
+#include <elf.h>
+
+#include <segments.h>
+#include <vmc1.h>
+
+/**
+ * Create a new empty segment struct and set to zero every field (TOBE checked)
+*/
+struct segment* seg_create(void) {
+    struct segment* seg;
+
+    seg = kmalloc(sizeof(struct segment));
+    KASSERT(seg != NULL);
+
+    seg->p_type = 0;
+    seg->p_offset = 0;
+    seg->p_vaddr = 0;
+    seg->p_filesz = 0;
+    seg->p_memsz = 0;
+    seg->p_permission = 0;
+
+    return seg;
+}
+
+/**
+ * Fill the empty segment struct given in the first parameter and checks it is still empty
+*/
+int seg_define(struct segment* seg, uint32_t p_type, uint32_t p_offset, uint32_t p_vaddr, uint32_t p_filesz, uint32_t p_memsz, uint32_t p_permission) {
+    
+    KASSERT(seg != NULL);
+    KASSERT(seg->p_type == 0);
+    KASSERT(seg->p_offset == 0);
+    KASSERT(seg->p_vaddr == 0);
+    KASSERT(seg->p_filesz == 0);
+    KASSERT(seg->p_memsz == 0);
+    KASSERT(seg->p_permission == 0);
+
+    seg->p_type = p_type;
+    seg->p_offset = p_offset;
+    seg->p_vaddr = p_vaddr;
+    seg->p_filesz = p_filesz;
+    seg->p_memsz = p_memsz;
+    seg->p_permission = p_permission;
+
+
+    return 0;
+}
+
+/**
+ * Free the memory of the given segment
+*/
+void seg_destroy(struct segment* seg) {
+
+    KASSERT(seg != NULL);
+    kfree(seg);
+}
+
+/**
+ * Define a stack segment having as base address the top one of our system due to
+ * its way of growing downwards.
+ * The size is a function of VMC1_STACKPAGES defined as constant in vmc1.h.
+ * Read and write operations are allowed.
+*/
+int seg_define_stack(struct segment* seg) {
+
+    KASSERT(seg != NULL);
+    KASSERT(seg->p_type == 0);
+    KASSERT(seg->p_offset == 0);
+    KASSERT(seg->p_vaddr == 0);
+    KASSERT(seg->p_filesz == 0);
+    KASSERT(seg->p_memsz == 0);
+    KASSERT(seg->p_permission == 0);
+
+    seg->p_type = PT_LOAD;          //unused
+    seg->p_offset = 0;              //unused
+    seg->p_vaddr = USERSTACK;       //base_addr as 0x8000000 (biggest addr of our VM system configuration)
+    seg->p_filesz = 0;              //unused
+    seg->p_memsz = USERSTACK - VMC1_STACKPAGES * PAGE_SIZE; //stack size is function of VMC1_STACKPAGES 
+    seg->p_permission = PF_R | PF_W;                        //read and write should be garanteed 
+
+    return 0;
+}
+
