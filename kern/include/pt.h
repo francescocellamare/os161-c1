3fac1cbc7333dd183efe254a9e75de4e54e3c028
Francesco Cellamare <francesco.pio.cellamare28@gmail.com>
2023-11-22 21:55:37 +0000

    added pagetable module

--- kern/include/pt.h
+++ kern/include/pt.h
@@ -1,21 +1,70 @@
 #ifndef _PT_H_
 #define _PT_H_
 
-/**
- * fields:
- * - n_page: number of pages in our page table
- * - pages: array sized with n_pages in tlb_init() having as i-th item a physical address (if it exists)
-*/
-struct pagetable {
+/*
+    addr (32 bits): p1 | p2 | d
+    p1: 10 bits, indexing inner page table
+    p2: 10 bits, indexing entries into the inner page table
+    d: 12 bits, offset
+*/ 
+#define SIZE_PT_OUTER 1024
+#define SIZE_PT_INNER 1024
+
+#define P1_MASK 0xFFC00000
+#define P2_MASK 0x003FF000
+#define D_MASK 0x00000FFF
+#define PFN_NOT_USED 0x00000000
+
+struct pt_inner_entry {
+    unsigned int valid;
+    paddr_t pfn;
+};
+struct pt_outer_entry {
+    unsigned int valid;
     unsigned int size;
-    paddr_t* pages;
+    struct pt_inner_entry* pages;
+};
+struct pt_directory {
+    unsigned int size;
+    struct pt_outer_entry* pages;
 };
 
 
-struct pagetable *pt = NULL;
+/**
+ * pt->
+ *      pages[p1]->
+ *      (if valid) pages[p2]->
+ *      (if valid) pfn
+*/
 
+/*
+    Create the directory pt having each inner table as NULL pointer till it's not used (VALID == 0) 
+*/
+struct pt_directory* pt_create(void);
 
-void pt_init(void);
-void pt_shutdown(void);
+/*
+    Static function which is called whenever a new inner pt is needed (so it becames valid)
+*/
+static void pt_define_inner(struct pt_directory* pt);
+
+/*
+    Free the whole structure
+*/
+void pt_destroy(struct pt_directory* pt);
+
+/*
+    Free the given inner table
+*/
+static void pt_destroy_inner(struct pt_outer_entry pt_inner);
+
+/*
+    Get the physical address having a virtual address, PFN_NOT_USED if it is not valid
+*/
+paddr_t pt_get_pa(struct pt_directory* pt, vaddr_t va);
+
+/*
+    Set the physical address having a virtual address, new inner table allocation is managed
+*/
+void pt_set_pa(struct pt_directory* pt, vaddr_t va, paddr_t pa);
 
 #endif
--- kern/vm/pt.c
+++ kern/vm/pt.c
@@ -20,3 +20,134 @@
  * It has 64 entries to be deactivated at each context switch [as_activate()]
  * 
 */
+static int get_p1(vaddr_t va) {
+    return va & P1_MASK;
+}
+
+static int get_p2(vaddr_t va) {
+    return va & P2_MASK;
+}
+
+static int get_d(vaddr_t va) {
+    return va & D_MASK;
+}
+
+
+static unsigned int get_npages(uint32_t memsize, vaddr_t va) {
+    unsigned int npages;
+
+    // segment's bytes + offset due to the va 
+    npages = memsize + (va & ~PAGE_FRAME);
+    // round the number of pages to the nearest integer (ie: 2 pages + x ==> 3, having x < PAGE_SIZE)
+    npages = ( (npages + PAGE_SIZE - 1) & PAGE_FRAME ) / PAGE_SIZE;
+
+    return npages;
+}
+
+struct pt_directory* pt_create(void) {
+    int i;
+
+    struct pt_directory *pt;
+
+    pt = kmalloc(sizeof(struct pt_directory));
+    KASSERT(pt != NULL);
+
+    pt->size = SIZE_PT_OUTER;
+    pt->pages = kmalloc(sizeof(pt_outer_entry)*SIZE_PT_OUTER);
+    KASSERT(pt->pages != NULL);
+
+    for(i = 0; i < pt->size; i++) {
+        pt->pages[i] = NULL;
+        pt->pages[i].valid = 0;
+    }
+
+    return pt;
+}
+
+
+static pt_destroy_inner(struct pt_outer_entry pt_inner) {
+    
+    KASSERT(pt_inner != NULL);
+    kfree(pt_inner.pages);
+}
+
+void pt_destroy(struct pt_directory* pt) {
+    int i;
+
+    KASSERT(pt != NULL);
+    for(i = 0; i < pt->size; i++) {
+        if(pt->pages[i] != NULL && pt->pages[i].valid) 
+            pt_destroy_inner(pt->pages[i]); 
+        
+    }
+    kfree(pt->pages);
+    kfree(pt);
+
+}
+
+static void pt_define_inner(struct pt_directory* pt, vaddr_t va) {
+    int index, i;
+
+    index = get_p1(va);
+
+    KASSERT(pt->pages[index].valid == 0);
+
+    pt->pages[index].size = SIZE_PT_INNER;
+    pt->pages[index].pages = kmalloc(sizeof(pt_inner_entry)*SIZE_PT_INNER);
+    KASSERT(pt->pages[index] != NULL);
+
+    for(i = 0; i < pt->pages[index].size; i++) {
+        pt->pages[index].pages[i].valid = 0;
+        pt->pages[index].pages[i].pfn = PFN_NOT_USED;
+    }
+}
+
+paddr_t pt_get_pa(struct pt_directory* pt, vaddr_t va) {
+    unsigned int p1, p2, d;
+
+    paddr_t pa;
+
+    p1 = get_p1(va);
+    KASSERT(p1 >= 0 && p1 < SIZE_PT_OUTER);
+
+    p2 = get_p2(va);
+    KASSERT(p2 >= 0 && p2 < SIZE_PT_INNER);
+
+    d = get_d(va);
+    KASSERT(d >= 0 && d < PAGE_SIZE);
+
+    if(pt->pages[p1].valid) {
+        if(pt->pages[p1].pages[p2].valid) {
+            pa = pt->pages[p1].pages[p2].pfn;
+        }
+        else {
+            return PFN_NOT_USED;
+        }
+    } else {
+        return PFN_NOT_USED;
+    }
+
+    return pa;
+}
+
+void pt_set_pa(struct pt_directory* pt, vaddr_t va, paddr_t pa) {
+    unsigned int p1, p2, d;
+
+    p1 = get_p1(va);
+    KASSERT(p1 >= 0 && p1 < SIZE_PT_OUTER);
+
+    p2 = get_p2(va);
+    KASSERT(p2 >= 0 && p2 < SIZE_PT_INNER);
+
+    d = get_d(va);
+    KASSERT(d >= 0 && d < PAGE_SIZE);
+
+    if(!pt->pages[p1].valid) {
+        pt_define_inner(pt, va);
+    }
+
+    // should be valid even after creation
+    KASSERT(pt->pages[p1].valid);
+    pt->pages[p1].pages[p2].valid = 1;
+    pt->pages[p1].pages[p2].pfn = pa;
+}
